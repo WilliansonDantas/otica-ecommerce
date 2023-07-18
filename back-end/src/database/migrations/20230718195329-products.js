a16f74c231813d653a59f87439caaff1f3acd945
Willianson Dantas <williansondantas@gmail.com>
2023-07-18 18:01:09 -0300

    feat: cria migration de products

---
+++ back-end/src/database/migrations/20230718195329-products.js
@@ -0,0 +1,33 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    return queryInterface.createTable("products", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      name: {
+        allowNull: false,
+        type: Sequelize.STRING(100),
+        unique: true,
+      },
+      url_image: {
+        allowNull: false,
+        type: Sequelize.STRING(200),
+        default: "",
+      },
+      price: {
+        allowNull: false,
+        type: Sequelize.DECIMAL(4, 2),
+      },
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    return queryInterface.dropTable("products");
+  },
+};
