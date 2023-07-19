b396bd4eeb65e8e4e8eb37cd9bbfd0679617dcbb
Willianson Dantas <williansondantas@gmail.com>
2023-07-19 17:38:03 -0300

    fix:exclui linhas comentadas

--- back-end/src/database/migrations/20230718195354-sales-products.js
+++ back-end/src/database/migrations/20230718195354-sales-products.js
@@ -1,6 +1,5 @@
 "use strict";
 
-/** @type {import('sequelize-cli').Migration} */
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     return queryInterface.createTable("sales_products", {
