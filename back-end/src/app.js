cae9291028cdf57ce389d5d73037b0d16cf84127
Willianson Dantas <williansondantas@gmail.com>
2023-07-19 21:50:58 -0300

    feat:insere estrutura para rotas

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -1,13 +1,16 @@
 const express = require("express");
 const cors = require("cors");
 
+const usersRouter = require("./routes/usersRouter");
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(usersRouter);
 
-app.get("/test", (req, res) => {
-  return res.json("Opa!!!");
-});
+// app.get("/test", (req, res) => {
+//   return res.json("Opa!!!");
+// });
 
 module.exports = app;
